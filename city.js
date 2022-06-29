2f61bda514e459d6df3e1bfee28410414a797229
chenhaibo <chb0413@163.com>
2022-06-29 10:55:23 +0800

    add 添加js无用注释

--- city.js
+++ city.js
@@ -1,3 +1,4 @@
+// 添加js注释无用
 var CityData = [{
         name: "北京",
         sub: [{name: "请选择"}, {name: "东城区"}, {name: "西城区"}, {name: "崇文区"}, {name: "宣武区"}, {name: "朝阳区"}, {name: "海淀区"}, {name: "丰台区"}, {name: "石景山区"}, {name: "房山区"}, {name: "通州区"}, {name: "顺义区"}, {name: "昌平区"}, {name: "大兴区"}, {name: "怀柔区"}, {name: "平谷区"}, {name: "门头沟区"}, {name: "密云县"}, {name: "延庆县"}, {name: "其他"}],
