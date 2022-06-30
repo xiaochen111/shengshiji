57d074ec66434d4131829989ef560ccbfd1211d7
chenhaibo <chb0413@163.com>
2022-06-30 11:37:31 +0800

    # 这是一个 14 个提交的组合。 # 这是第一个提交说明：
    add:add some after v1
    
    # 这是提交说明 #2：
    
    feat:ee
    
    # 这是提交说明 #3：
    
    fix:mastar
    
    # 这是提交说明 #4：
    
    fix:aaa
    
    # 这是提交说明 #5：
    
    feat:OK了
    
    # 这是提交说明 #6：
    
    fix:1
    
    # 这是提交说明 #7：
    
    fix:2
    
    # 这是提交说明 #8：
    
    feat:OK了
    
    # 这是提交说明 #9：
    
    fix:1
    
    # 这是提交说明 #10：
    
    fix:2
    
    # 这是提交说明 #11：
    
    fix:pre
    
    # 这是提交说明 #12：
    
    feat:OK了
    
    # 这是提交说明 #13：
    
    fix:1
    
    # 这是提交说明 #14：
    
    fix:2

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+/*
+ * @Author: chenhaibo-chb0413@163.com
+ * @Date: 2022-08-17 13:28:43
+ * @Description: 
+ */
 console.log('test')
 
 
@@ -6,3 +11,20 @@ const b = 1;
 const c = 1;
 
 const d = 2
+// mastar
+
+// aaaa mastar 
+// 在feature aa分支下 完成了任务
+// 1
+// 2
+// 在feature aa分支下 完成了任务
+// 1
+// 2
+
+
+
+
+const dd = 'dev-pre'
+// 在feature aa分支下 完成了任务
+// 1
+// 2
