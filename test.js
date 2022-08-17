bd9f835f6b30af1fb5196c198082980d083166a3
chenhaibo <chb0413@163.com>
2022-08-17 14:58:32 +0800

    fix:mastar

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+/*
+ * @Author: chenhaibo-chb0413@163.com
+ * @Date: 2022-08-17 14:57:43
+ * @Description: 
+ */
 console.log('test')
 
 
@@ -6,3 +11,4 @@ const b = 1;
 const c = 1;
 
 const d = 2
+// mastar
