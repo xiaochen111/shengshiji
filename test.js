acfedcf6ff5e345ac90898a1979971d1c23ca8e5
chenhaibo <chb0413@163.com>
2022-08-17 14:18:30 +0800

    feat:OK了

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+/*
+ * @Author: chenhaibo-chb0413@163.com
+ * @Date: 2022-08-17 13:28:43
+ * @Description: 
+ */
 console.log('test')
 
 
@@ -6,3 +11,4 @@ const b = 1;
 const c = 1;
 
 const d = 2
+// 在feature aa分支下 完成了任务
